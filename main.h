8e75259404ab486f61e7d8ee6e064f70124e0e55
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-14 04:11:16 -0800

    Added integer functions

--- main.h
+++ main.h
@@ -1,7 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+/* function declerations/prototypes */
 
 int _printf(const char *format, ...);
 int mr_putchar(char c);
+int i_specifier(va_list digit);
+int d_specifier(va_list digit);
+
+/* structures and typedef*/
+
 
 #endif
