84472570a89acf567b241fbccfae0ea4fcf27825
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-14 05:07:55 -0800

    update

--- mr_putchar.c
+++ mr_putchar.c
@@ -1,9 +1,11 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
  * mr_putchar - this is a function that gives output
  * @a: parameter
  * Return: any
  */
+
 int mr_putchar(char a)
 {
 	return (write(1, &a, 1));
